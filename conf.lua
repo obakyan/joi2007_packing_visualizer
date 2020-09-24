d9d92581078e6f4a53172f5ff08d2a3c290827a6
obakyan <obakyan@gmail.com>
2020-09-24 23:12:24 +0900

    add save mode. add special change mode

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.version = "11.2"
   t.window.resizable = false
-  t.window.width = 500
+  t.window.width = 700
   t.window.height = 500
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 local mfl, mce = math.floor, math.ceil
-local mmi = math.min
+local mmi, mma = math.min, math.max
 local msq = math.sqrt
 local rnd = math.random
 local xc, yc = {}, {}
 local n = 6
 local curscore = 0
 local step = 0
+local maxscore = 0
+local max_xc, max_yc = {}, {}
 
 local function getlen(i, j)
   return msq((xc[i] - xc[j]) * (xc[i] - xc[j]) + (yc[i] - yc[j]) * (yc[i] - yc[j]))
@@ -42,18 +44,50 @@ function love.load()
   curscore = getscore()
 end
 
+local function special_challenge()
+  for im = 1, n do
+    local idx = rnd(1, n)
+    local px, py = xc[idx], yc[idx]
+    local range = 0.1
+    xc[idx] = mma(0, mmi(1, xc[idx] + range * (rnd() - 0.5)))
+    yc[idx] = mma(0, mmi(1, yc[idx] + range * (rnd() - 0.5)))
+    local nxtscore = getscore()
+    curscore = nxtscore
+    xc[idx], yc[idx] = px, py
+    if maxscore < curscore then
+      maxscore = curscore
+      for i = 1, n do
+        max_xc[i] = xc[i]
+        max_yc[i] = yc[i]
+      end
+    end
+  end
+end
+
 local function challenge()
   step = step + 1
+  if step % 1000000 == 0 then
+    special_challenge()
+    step = 0
+    return
+  end
   local idx = rnd(1, n)
   local px, py = xc[idx], yc[idx]
-  local range = 0.05
-  xc[idx] = xc[idx] + range * (rnd() - 0.5)
-  yc[idx] = yc[idx] + range * (rnd() - 0.5)
+  local range = 0.02
+  xc[idx] = mma(0, mmi(1, xc[idx] + range * (rnd() - 0.5)))
+  yc[idx] = mma(0, mmi(1, yc[idx] + range * (rnd() - 0.5)))
   local nxtscore = getscore()
   if curscore <= nxtscore then
     curscore = nxtscore
   else
     xc[idx], yc[idx] = px, py
+  end
+  if maxscore < curscore then
+    maxscore = curscore
+    for i = 1, n do
+      max_xc[i] = xc[i]
+      max_yc[i] = yc[i]
+    end
   end
 end
 function love.update(dt)
@@ -66,10 +100,30 @@ end
 
 function love.draw()
   local w = 500
+  love.graphics.setColor(1, 1, 1)
   love.graphics.print("" .. curscore, 0, 0)
-  love.graphics.print("" .. step, 0, 30)
+  -- love.graphics.print("" .. step, 0, 30)
   for i = 1, n do
     local x, y = xc[i], yc[i]
     love.graphics.circle("line", x * w, y * w, curscore * w)
+  end
+  love.graphics.setColor(0.1, 0, 0.3)
+  love.graphics.rectangle("fill", 500, 0, 200, 500)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print("max score", 500, 0)
+  love.graphics.print(tostring(maxscore), 500, 24)
+  love.graphics.print("press [s] to save", 500, 48)
+end
+
+function love.keypressed(key, scancode, rep)
+  if key == "s" then
+    local f = io.open("out.lua", "w")
+    f:write("-- https://github.com/obakyan/joi2007_packing_visualizer\n")
+    for i = 1, n do
+      f:write("print(\""
+        .. mfl(xc[i] * 100000000 + 0.5) .. " "
+        .. mfl(yc[i] * 100000000 + 0.5) .. "\")\n")
+    end
+    f:close()
   end
 end
